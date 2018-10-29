36adc4fcf9c6e6c7f0df906fd528362f2ec3b809
Luis Fernando Chacon Fajardo <lf.chacon10@uniandes.edu.co>
2018-10-29 18:58:48 -0500

    Guarda los datos

--- P2.py
+++ P2.py
@@ -1,1 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Punto 1: almacenamiento de datos.
+dataInc= np.genfromtxt("incompletos.dat", delimiter=",")
+dataSig= np.genfromtxt("signal.dat", delimiter=",")
+
 
