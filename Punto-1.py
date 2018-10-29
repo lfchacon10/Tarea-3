495212187acd2a8cd997c31aac1d490a9c880af3
Luis Fernando Chacon Fajardo <UANDES\lf.chacon10@fisicaez12218l.ad.uniandes.edu.co>
2018-10-29 17:59:59 -0500

    Aparente avance pero nada funciona (aparentemente)

--- Punto-1.py
+++ Punto-1.py
@@ -33,38 +33,34 @@ for i in range(len(c)):
 
 matriz=np.array(matriz).T
 
-print (matriz)
-
-#Cree una funcion para Sumar y para dar el mean ya que con NP por algún extraño motivo no me funciono. Es posible que sea por los ' ' que prevalecene n el archivo incluso despues de convertir la matriz a float.
-def sumYMean( x ):
-    sum =0
-    for i in range(len(x)):
-        sum+= float(x[i])
-    mean = sum / len(x)
-    return sum, mean
 
 #Covarianza
-
 def covarianza (x,y):
-    sumX, meanX = sumYMean(x)
-    sumY, meanY = sumYMean(y)
-    for i in range(len(x)):
-        for j in range(len(y)):
-            sumaCova= (float(x[i])-meanX)*(float(y[j])- meanY)
-    return sumaCova/ len(x)
-
+	for i in range(len(x)):
+		for j in range(len(y)):
+			sumaCova= (x[i]-np.mean(x))*(y[j]- np.mean(y))
+	return sumaCova/ len(x)
 
 #Matriz covarianza. No vamos a tener en cuenta la variable ID ya que no tiene nada ver con tener cancer. Se dejara la variable Diagnosis para poder ver que variable es la que mas influye en esta diagnosis.
 
-cantidadVariables = len(matriz[:]) -1
+cantidadVariables = len(matriz[:])
 matrizCova= np.zeros((cantidadVariables, cantidadVariables) )
 for i in range (cantidadVariables):
     for j in range (cantidadVariables):
-        if( i == 31):
-            matrizCova[i][j] = covarianza( matriz[i+1], matriz[i+1])
-        else: matrizCova[i][j] = covarianza( matriz[i+1], matriz[j+1])
+        if( i == 32):
+            matrizCova[i][j] = covarianza( matriz[i], matriz[i])
+        else: matrizCova[i][j] = covarianza( matriz[i], matriz[j])
+
+print ("MIA ", matrizCova, " CALCULADO POR NP ",np.cov(matriz) )
 
 
+#Punto 3: calcula autovalores y autovectores.
+
+eigenValues = np.eig( matrizCova)
+eigenVectors = np.eig( matrizCova)
+
+#for i in range (len (eigenValues)):
+#	print ( "ID EigenValue: "eigenValues[i], "ID EigenVector: ", eigenVectors[i], "\n Radius EigenValue: ", eigenValues[i],  " Radius EigenVector: ",eigenVectors[i], "\n Texture EigenValue: ", eigenValues[i],  " Texture EigenVector: ",eigenVectors[i], "\n Perimeter EigenValue: ", eigenValues[i],  " Perimeter EigenVector: ",eigenVectors[i], "\n Area EigenValue: ", eigenValues[i],  " Area EigenVector: ",eigenVectors[i], "\n Smoothness EigenValue: ", eigenValues[i],  " Smoothness EigenVector: ",eigenVectors[i], "\n Compactness EigenValue: ", eigenValues[i],  " Compactness EigenVector: ",eigenVectors[i], "\n Concavity EigenValue: ", eigenValues[i],  " Concavity EigenVector: ",eigenVectors[i], "\n Concave EigenValue: ", eigenValues[i],  " Concave EigenVector: ",eigenVectors[i], "\n Symmetry EigenValue: ", eigenValues[i],  " Symmetry EigenVector: ",eigenVectors[i],  "\n Fractal dimension EigenValue: ", eigenValues[i],  " Fractal dimension EigenVector: ",eigenVectors[i] )
 
 
 
