9b2a36d8c9e814f3a543d5ff387a11ba240c16e0
Luis Fernando Chacon Fajardo <lf.chacon10@uniandes.edu.co>
2018-10-30 07:48:23 -0500

    Salen los datos y además grafica bien

--- P2.py
+++ P2.py
@@ -2,7 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Punto 1: almacenamiento de datos.
-dataInc= np.genfromtxt("incompletos.dat", delimiter=",")
-dataSig= np.genfromtxt("signal.dat", delimiter=",")
+dataInc= np.loadtxt("incompletos.dat", delimiter=",")
+dataSig= np.loadtxt("signal.dat", delimiter=",")
 
+#Punto 2: gráfica signal y almacenamiento
+xS= np.loadtxt("signal.dat", delimiter=",", usecols=(0,))
+yS= np.loadtxt("signal.dat", delimiter=",", usecols=(1,))
 
+xI= np.loadtxt("incompletos.dat", delimiter=",", usecols=(0,))
+yI= np.loadtxt("incompletos.dat", delimiter=",", usecols=(1,))
+
+plt.plot(xI,yI)
+plt.show()
