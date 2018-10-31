7e552e4efdcd41ab715190121bbfd4a45f18c2cd
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-10-31 16:07:28 -0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -9,9 +9,13 @@ tamano=len(img)
 f=500.0
 dt=1/(f*32)
 
+#Punto 2: Transformada de Fourier.
 img_fourier=fftpack.fft2(img)
 plt.imshow(abs(img_fourier))
 plt.show()
+
+#Punto 3: filtro de la imagen.  
+img[where(img>3500)] = 0
 
 
 freqq=fftpack.fftfreq(tamano,dt)
