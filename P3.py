d636952d31d3bd76c746bd1fcd357ceefe869b72
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-10-31 15:50:19 -0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -3,3 +3,12 @@ from scipy import fftpack
 #Punto 1: Guarda la imagen.
 img = plt.imread('arbol.png')
 
+tamano=len(img)
+f=500.0
+dt=1/(f*32)
+
+prft=fftpack.fft2(img)
+freqq=fftpack.fftfreq(tamano,dt)
+shiftt=fftpack.fftshift(prft)
+ps2D=abs(shiftt)
+
