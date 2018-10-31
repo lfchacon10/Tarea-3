350447f74d0a159e24353a3260f257d4326205b0
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-10-31 16:02:53 -0500

    Update P3.py
    Muestra transformada de fourier

--- P3.py
+++ P3.py
@@ -1,4 +1,6 @@
 from scipy import fftpack
+import numpy as np
+
 
 #Punto 1: Guarda la imagen.
 img = plt.imread('arbol.png')
@@ -7,7 +9,11 @@ tamano=len(img)
 f=500.0
 dt=1/(f*32)
 
-prft=fftpack.fft2(img)
+img_fourier=fftpack.fft2(img)
+plt.imshow(abs(img_fourier))
+plt.show()
+
+
 freqq=fftpack.fftfreq(tamano,dt)
 shiftt=fftpack.fftshift(prft)
 ps2D=abs(shiftt)
