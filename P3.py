42150c46a88a0711f049ffd3759b433c2185a608
lfchacon10 <lf.chacon10@uniandes.edu.co>
2018-10-31 19:01:29 -0500

    Mejoro la imagen pero sigue viendose fea

--- P3.py
+++ P3.py
@@ -1,34 +1,42 @@
-from scipy.fftpack import fft, ifft
+from scipy import fftpack
 from matplotlib.image import imread
 import numpy as np
 import matplotlib.pyplot as plt
 
 #Punto 1: Guarda la imagen.
-img = imread('arbol.png')
-plt.imshow(img)
-plt.show()
-#Punto 2: Transformada de Fourier.
-imgF = np.fft.fft2(img)
-fig= plt.gcf()
-plt.imshow(np.abs(imgF))
-plt.grid()
-plt.savefig("LF.png")
-plt.show()
-
-#Punto 3: filtro de la imagen.  
-img[np.where(img>3500)] = 0
-
-#Punto 4: log normal.
-logCasual = np.log(img_Fourier)
-fig= plt.gcf()
-plt.imshow(logCasual)
-plt.show()
-
-
+img = imread("arbol.png")
 tamano=len(img)
 f=500.0
 dt=1/(f*32)
-freqq=fftpack.fftfreq(tamano,dt)
-shiftt=fftpack.fftshift(prft)
-ps2D=abs(shiftt)
+
+#Punto 2: Transformada de Fourier.
+imgF = fftpack.fft2(img, shape= img.shape[:2],axes=(0,1))
+plt.savefig("ChaconLuis_FT2D.pdf")
+
+
+#Punto 3: filtro de la imagen.  
+freqq = fftpack.fftfreq(tamano, dt)
+shiftt= fftpack.fftshift(imgF)
+ps = abs(shiftt)
+plt.imshow(imgF.real**2 + imgF.imag**2)
+
+freq_cut = 3500.0
+imgF[abs(freqq)>freq_cut] = 0
+logNormal = np.log(abs(imgF))
+plt.imshow(logNormal)
+plt.show()
+
+
+imgFinal= fftpack.ifft2(imgF).real
+plt.imshow(imgFinal, cmap="gray")
+plt.show()
+
+
+cleanf=np.fft.ifft2(img,axes=(0, 1))
+cleanr=cleanf.real
+plt.figure(figsize=(10,10))
+plt.imshow(cleanr)
+plt.show()
+#Punto 4: log normal.
+
 
